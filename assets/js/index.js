986a52daaf898d24988888d515ed8fc930f7dfe5
beliberda <anton.nurgaliev@mail.ru>
2023-06-29 19:28:23 +1000

    saaa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+import profession from "./massives/profession.js";
+import nameMass from "./massives/name.js";
 
 let person = {
     name: null,
@@ -11,25 +13,26 @@ let person = {
 
 }
 
-
 let createCharacter = document.getElementById('create-character')
 let title = document.querySelector('.main-title')
 let personEl = document.querySelector('.person')
 
-let nameMass = ['Анна', 'Антон', 'Зелебоба', 'Умпа Лумп', "Витя", "Петя", "Маша", "Г.Л.А.Ш.А"]
-
+function Random(max,min) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
 
 let personItems = document.querySelectorAll('.person-item')
-console.log(personItems);
+
 function Generate() {
     person.name = nameMass[Math.floor(Math.random() * (nameMass.length - 1 - 0 + 1) + 0)]
     person.age = Math.floor(Math.random() * (10 - 85 + 1) + 85)
-
-    person.work_experience = Math.floor(Math.random() * (10 - 20 + 1) + 20)
+    person.profession = profession[Math.floor(Math.random() * (profession.length - 1 - 0 + 1) + 0)]
+    person.work_experience = Random(person.age/1.2,0)
 
 
     personItems[0].innerHTML=`Имя: ${person.name}`
     personItems[1].innerHTML=`Возраст: ${person.age}`
+    personItems[2].innerHTML=`Профессия: ${person.profession}`
     personItems[3].innerHTML=`Стаж работы: ${person.work_experience}`
 
 
@@ -50,8 +53,29 @@ createCharacter.onclick = () => {
 
 }
 
+let radio = document.querySelector('.music')
+let music = document.querySelector('audio')
+let control_range = document.querySelector('.range')
+let isPlay = true
 
+control_range.addEventListener('change', ()=>{
+    music.volume = control_range.value/100
+    console.log(control_range.value);
+})
 
+radio.onclick = () => {
+    if (isPlay) {
+        music.pause()
+        console.log('Остановили');
+        isPlay = false
+    } else {
+        music.play()
+        console.log('Запустили');
+        isPlay = true
+    }
+    
+
+}
 /*
 Имя
 Возраст
